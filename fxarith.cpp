50e0ed544911cd00ae8a4cadc4b35478b2d8b92a
me <me@home>
2025-04-28 16:19:43 +0200

    fxarith.cpp: More constexpr.

--- fxarith.cpp
+++ fxarith.cpp
@@ -52,7 +52,7 @@ public:
 
     constexpr FBit () : v(0) {}
 
-    FBit (double f)
+    constexpr FBit (double f)
     {
         bool fsign = 0;
         if (S)
@@ -73,28 +73,35 @@ public:
 
     static constexpr FBit gen (T v)
     {
-        //printf ("{v=%d}",(int)v);
         FBit x;
         x.v = extend (v);
         return x;
     }
 
-    I asInt () const
+    constexpr I asInt () const
     {
         return (I) v;
     }
 
-    explicit operator double () const
+    explicit constexpr operator double () const
     {
-        return ldexp (asInt(), -FBIT);
+        return std::ldexp (asInt(), -FBIT);
     }
 
-    explicit operator float () const
+    explicit constexpr operator float () const
     {
         return ldexpf (asInt(), -FBIT);
     }
 
-    bool sign () const
+    template<bool xS, int xIBIT, int xFBIT>
+    constexpr operator FBit<xS, xIBIT, xFBIT> () const
+    {
+        T v = asInt();
+        v = xFBIT > FBIT ? v << (xFBIT - FBIT) : v >> (FBIT - xFBIT);
+        return FBit<xS, xIBIT, xFBIT>::gen (v);
+    }
+
+    constexpr bool sign () const
     {
         return S ? v & signmask : 0;
     }
@@ -106,12 +113,12 @@ public:
     bool operator <  (const FBit &y) const { return extend(v) <  extend(y.v); }
     bool operator >  (const FBit &y) const { return extend(v) >  extend(y.v); }
 
-    FBit operator - () const
+    constexpr FBit operator - () const
     {
         return gen (- extend (v));
     }
 
-    FBit operator + (const FBit &y) const
+    constexpr FBit operator + (const FBit &y) const
     {
         return gen (extend (v + y.v));
     }
@@ -126,24 +133,24 @@ public:
         *this = *this * y;
     }
 
-    FBit operator - (const FBit &y) const
+    constexpr FBit operator - (const FBit &y) const
     {
         return gen (extend (v - y.v));
     }
 
-    FBit operator >> (int i) const
+    constexpr FBit operator >> (int i) const
     {
         T t = extend (v);
         return gen (extend (t >> i));
     }
 
-    FBit operator << (int i) const
+    constexpr FBit operator << (int i) const
     {
         T t = extend (v);
         return gen (extend (t << i));
     }
 
-    FBit operator * (const FBit &y) const
+    constexpr FBit operator * (const FBit &y) const
     {
         T a = v;
         T b = y.v;
@@ -171,14 +178,14 @@ public:
         return gen (ab);
     }
 
-    FBit sqrt () const
+    constexpr FBit sqrt () const
     {
         double d = std::ldexp ((double) asInt(), FBIT);
         double q = std::sqrt (d);
         return FBit::gen ((I) q);
     }
 
-    FBit absdiff (const FBit &y) const
+    constexpr FBit absdiff (const FBit &y) const
     {
         return *this > y ? *this - y : y - *this;
     }
@@ -206,19 +213,19 @@ using accum = FBit<1, 16, 15>;
 using ufract = FBit<0, 0, 16>;
 using uhfract = FBit<0, 0, 8>;
 
-uaccum  ukbits  (int i) { return uaccum::gen (i);  }
-ufract  urbits  (int i) { return ufract::gen (i);  }
-uhfract uhrbits (int i) { return uhfract::gen (i); }
+constexpr uaccum  ukbits  (int i) { return uaccum::gen (i);  }
+constexpr ufract  urbits  (int i) { return ufract::gen (i);  }
+constexpr uhfract uhrbits (int i) { return uhfract::gen (i); }
 
-unsigned bitsuk  (uaccum x)  { return x.asInt(); }
-unsigned bitsur  (ufract x)  { return x.asInt(); }
-unsigned bitsuhr (uhfract x) { return x.asInt(); }
-
-template<typename T>
-T fxbits (typename T::I i) { return T::gen (i); }
+constexpr unsigned bitsuk  (uaccum x)  { return x.asInt(); }
+constexpr unsigned bitsur  (ufract x)  { return x.asInt(); }
+constexpr unsigned bitsuhr (uhfract x) { return x.asInt(); }
 
 template<typename T>
-typename T::I bitsfx (T x) { return x.asInt(); }
+constexpr T fxbits (typename T::I i) { return T::gen (i); }
+
+template<typename T>
+constexpr typename T::I bitsfx (T x) { return x.asInt(); }
 
 template<typename T>
 static inline float fxtof (T x) { return (float) x; }
